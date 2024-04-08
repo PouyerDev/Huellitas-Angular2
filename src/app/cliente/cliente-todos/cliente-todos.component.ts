40dc6a26cf6cbf5a03ea39377a2f16657c55e09a
Natalia Mejia <nataliamejia@javeriana.edu.co>
2024-04-08 12:06:12 -0500

    Correccion imports

--- src/app/cliente/cliente-todos/cliente-todos.component.ts
+++ src/app/cliente/cliente-todos/cliente-todos.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/model/cliente';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-cliente-todos',
   templateUrl: './cliente-todos.component.html',
   styleUrls: ['./cliente-todos.component.css']
 })
-export class ClienteTodosComponent {
+export class ClienteTodosComponent implements OnInit {
+  clientes: Cliente[] = [];
 
+  constructor(private clienteService: ClienteService) { }
+
+  ngOnInit(): void {
+    this.loadClientes();
+  }
+
+  loadClientes(): void {
+    this.clienteService.getAllClientes().subscribe(clientes => {
+      this.clientes = clientes;
+    });
+  }
+  verDetalle(clienteId: number): void {
+    // Here you can navigate to the detailed view of the client
+    // For example, using Angular Router:
+    // this.router.navigate(['/clientes', clienteId]);
+  }
 }
