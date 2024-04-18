258311282c1478f4f3d7558084bcc2d409a17f17
Natalia Mejia <nataliamejia@javeriana.edu.co>
2024-04-18 09:09:06 -0500

    Dashboard avances

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -9,16 +9,24 @@ export class AuthService {
 
     private currentUserSubject: BehaviorSubject<string> = new BehaviorSubject<string>('');
     currentUser: Observable<string> = this.currentUserSubject.asObservable();
-
+    private currentUserSubjectCedula: BehaviorSubject<string> = new BehaviorSubject<string>('');
+    currentUserCedula: Observable<string> = this.currentUserSubjectCedula.asObservable();
+    
     constructor(
        
     ) { }
 
     setCurrentUser(user: string): void {
-        this.currentUserSubject.next(user);
+        this.currentUserSubject.next('dev');
     }
 
     getCurrentUser(): Observable<string> {
         return this.currentUser;
+    }
+    getCurrentUserCedula(): string {
+        return this.currentUserCedula.toString();
+    }
+    setCurrentUserCedula(cedula: string): void {
+        this.currentUserSubject.next(cedula);
     }
 }
