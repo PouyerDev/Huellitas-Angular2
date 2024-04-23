f9dddae28979c1f7973f138a8c929fe22e875eaa
Natalia Mejia <nataliamejia@javeriana.edu.co>
2024-04-23 16:07:38 -0500

    Correccion Landing

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.css']
 })
-export class LandingComponent {
+export class LandingComponent implements OnInit{
+  ngOnInit() {
+    document.addEventListener("DOMContentLoaded", () => {
+      const slidesContainer = document.getElementById("slides-container")!;
+      const slides = document.querySelectorAll(".slide")!;
+      const prevButton = document.getElementById("slide-arrow-prev")!;
+      const nextButton = document.getElementById("slide-arrow-next")!;
+      let currentSlideIndex = 0;
 
+      nextButton.addEventListener("click", () => {
+        const slideWidth = slides[0].clientWidth;
+        currentSlideIndex++;
+        if (currentSlideIndex >= slides.length) {
+          currentSlideIndex = 0;
+        }
+        slidesContainer.scrollLeft = currentSlideIndex * slideWidth;
+      });
+
+      prevButton.addEventListener("click", () => {
+        const slideWidth = slides[0].clientWidth;
+        currentSlideIndex--;
+        if (currentSlideIndex < 0) {
+          currentSlideIndex = slides.length - 1;
+        }
+        slidesContainer.scrollLeft = currentSlideIndex * slideWidth;
+      });
+    });
+  }
+
+  scrollTo(section: string) {
+    const element = document.getElementById(section);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  slideNext() {
+    const slidesContainer = document.getElementById("slides-container")!;
+    const slides = document.querySelectorAll(".slide");
+    const slideWidth = slides[0].clientWidth;
+    let currentSlideIndex = Math.floor(slidesContainer.scrollLeft / slideWidth) + 1;
+
+    if (currentSlideIndex >= slides.length) {
+      currentSlideIndex = 0;
+    }
+
+    slidesContainer.scrollLeft = currentSlideIndex * slideWidth;
+  }
+
+  slidePrev() {
+    const slidesContainer = document.getElementById("slides-container")!;
+    const slides = document.querySelectorAll(".slide");
+    const slideWidth = slides[0].clientWidth;
+    let currentSlideIndex = Math.floor(slidesContainer.scrollLeft / slideWidth) - 1;
+
+    if (currentSlideIndex < 0) {
+      currentSlideIndex = slides.length - 1;
+    }
+
+    slidesContainer.scrollLeft = currentSlideIndex * slideWidth;
+  }
 }
