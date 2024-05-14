bc68b14ed92315f0b9d93bfd75ec94560cb95fe9
María Andrea Méndez <101602812+mariaa-mendez@users.noreply.github.com>
2024-05-14 16:41:25 -0500

    konami

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { AuthService } from './service/auth.service';
@@ -9,6 +9,8 @@ import { AuthService } from './service/auth.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  sequenceMatched = false;
+
   constructor(
     private authService: AuthService,
     private router: Router
@@ -36,4 +38,23 @@ export class AppComponent implements OnInit {
   isLoginPageOrLanding(): boolean {
     return this.router.url === '/login' || this.router.url === '/landing' || this.router.url === '/';
   }
+   // Agregar la detección de la secuencia de teclas
+ private sequence: string[] = []; 
+ private secretSequence = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'KeyB', 'KeyA']; 
+
+ @HostListener('document:keydown', ['$event'])
+ handleKeyboardEvent(event: KeyboardEvent) {
+   this.sequence.push(event.code);
+   this.sequence = this.sequence.slice(-this.secretSequence.length);
+
+   if (this.sequence.join('') === this.secretSequence.join('')) {
+     console.log('Secuencia secreta ingresada!');
+     // Realizar la acción deseada aquí, por ejemplo:
+     //this.scrollTo('seccion-secreta');
+     this.sequenceMatched = true;
+     window.location.href = 'https://youtu.be/dQw4w9WgXcQ';
+
+     // O puedes llamar a cualquier otra función que desees ejecutar cuando se ingrese la secuencia secreta.
+   }
+ }
 }
